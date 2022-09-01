4eba5a58862acff5ece4594e91a6ba8a1e6e1c33
Nicolás Droguett <nicolas.droguett@elev.ga.ntig.se>
2022-09-01 18:06:13 +0200

    Initial commit

---
+++ LuckTextUppgift/Program.cs
@@ -0,0 +1,17 @@
+﻿string name;
+string bodyPart;
+string liquid;
+
+Console.WriteLine("What is your name?");
+name = Console.ReadLine();
+
+Console.WriteLine("Please name a body part:");
+bodyPart = Console.ReadLine();
+
+Console.WriteLine("Please name a liquid:");
+liquid = Console.ReadLine();
+
+Console.WriteLine($"{name} decided to stay home from school because their {bodyPart} hurts and has to drink {liquid} to recover from their injury");
+
+Console.ReadLine();
+
